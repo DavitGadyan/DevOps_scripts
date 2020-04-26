e62c24a71d5a76db13ccd372c4e68978f16b6be9
unknown <davidgadyan92@gmail.com>
2020-04-26 19:27:03 +0400

    testing code

--- Pytest/test_func5.py
+++ Pytest/test_func5.py
@@ -1,4 +1,4 @@
-import pytest
+himport pytest
 
 # Fill in with a context manager that will silence the ValueError
 with pytest.raises(ValueError):
@@ -67,3 +67,40 @@ def test_on_one_row():
     expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
     # Check if the raised ValueError contains the correct message
     assert exc_info.match(expected_error_msg)
+
+
+import pytest
+from preprocessing_helpers import row_to_list
+
+def test_on_no_tab_no_missing_value():    # (0, 0) boundary value
+    # Assign actual to the return value for the argument "123\n"
+    actual = row_to_list("123\n")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_two_tabs_no_missing_value():    # (2, 0) boundary value
+    actual = row_to_list("123\t4,567\t89\n")
+    # Complete the assert statement
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_one_tab_with_missing_value():    # (1, 1) boundary value
+    actual = row_to_list("\t4,567\n")
+    # Format the failure message
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+
+
+# separating functions inside test file with classes
+
+import pytest
+import numpy as np
+
+from models.train import split_into_training_and_testing_sets
+
+# Declare the test class
+class TestSplitIntoTrainingAndTestingSets(object):
+    # Fill in with the correct mandatory argument
+    def test_on_one_row(self):
+        test_argument = np.array([[1382.0, 390167.0]])
+        with pytest.raises(ValueError) as exc_info:
+            split_into_training_and_testing_sets(test_argument)
+        expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
+        assert exc_info.match(expected_error_msg)
